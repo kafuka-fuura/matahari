7c8b314b46d6136aa0d665c3fab6e7f896b335f5
kafuka-fuura <abby.s.nugraha@gmail.com>
2019-06-30 14:13:16 +0700

    Initial page setup

---
+++ pages/header.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+	render() {
+		return(
+			<h2>Hello lo lo lo,,</h2>
+		);
+	}
+}
+
+export default Header;
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Header from "./header";
+
+const App = () => (
+	<div><Header /></div>
+);
+
+export default App;
