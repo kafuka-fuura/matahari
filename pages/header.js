d069c3b4520f601c9f628c1c15a650a987d84bee
kafuka-fuura <abby.s.nugraha@gmail.com>
2019-07-02 09:39:35 +0700

    Base layout

--- pages/header.js
+++ pages/header.js
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
 
+import { Row, Col, Form, FormGroup, Label, Input } from 'reactstrap';
+
+
 class Header extends Component {
 	render() {
 		return(
-			<h2>Hello lo lo lo,,</h2>
+			<Row>
+				<Col xs="12">
+					<Form className="header-search">
+				        <FormGroup>
+							<Label for="search">Search</Label>
+							<Input type="text" 
+									name="q" 
+									id="search" 
+									placeholder="Search Here.."
+									onChange="{ this.testMethod( value ) }" />
+				        </FormGroup>
+			        </Form>
+				</Col>
+			</Row>
 		);
+	}
+
+	testMethod( a ) {
+		console.log( a );
 	}
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
 import React from "react";
+import "../static/css/styles.less";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {
+	Row,
+	Col
+} from "reactstrap";
+
 import Header from "./header";
 
 const App = () => (
-	<div><Header /></div>
+	<div className="main-container">
+		<div className="header-container container">
+			<Header />
+		</div>
+		<div className="body-container noresult">
+			
+		</div>
+	</div>
 );
 
 export default App;
